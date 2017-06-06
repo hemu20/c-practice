737e94c443f9db96205d5cb7c6392fdff462025b
hemu20 <drive2Hemanth@gmail.com>
2017-06-06 09:09:13 +0530

    Create arthmetic.c

---
+++ arthmetic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+int a,b,c; 
+printf("enter the number");
+scanf("%d,%d",*&a,&b);
+c=a+b;
+c=a-b;
+c=a*b;
+printf("addition is%d",c);
+printf("subb is %d",c);
+printf("mul is %d",c);
+}
